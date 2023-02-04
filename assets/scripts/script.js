cd238a1129dd6e8d4f220e26aa7b548b13cc465d
brianach <brianocualain@outlook.ie>
2023-02-04 12:59:58 +0000

    javascrip event listener added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,16 @@
-console.log("Connected");
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "play-button") {
+                alert("You clicked a play-button!");
+            } else {
+                alert(`Something went wrong!`);
+            }
+        });
+    }
+});
