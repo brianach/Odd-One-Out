b0940e4f37d6350d49a9ce0dde170c58a908fd8d
brianach <brianocualain@outlook.ie>
2023-02-04 16:53:12 +0000

    create icon arrays

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,16 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-var clouds = ['fa-solid fa-cat', 'fa-solid fa-dog', 'fa-solid fa-horse', 'fa-solid fa-fish'] ;
+var animals = ['fas fa-cat', 'fas fa-dog', 'fas fa-horse', 'fas fa-fish'] ;
+var clouds = ['fas fa-cloud', 'fas fa-cloud-showers-heavy', 'fas fa-cloud-rain', 'fas fa-sun'];
+var motor = ['fas fa-car-side', 'fas fa-truck', 'fas fa-truck-pickup', 'fas fa-motorcycle'];
+var tools =  ['fas fa-hammer', 'fas fa-wrench', 'fas fa-trowel', 'fas fa-pencil'];
+var alpha =  ['fas fa-a', 'fas fa-i', 'fas fa-u', 'fas fa-z'];
+var homes =  ['fas fa-house-circle-xmark', 'fas fa-house-circle-exclamation', 'fas fa-house-circle-check', 'fas fa-house'];
+var foods =  ['fas fa-egg', 'fas fa-apple-whole', 'fas fa-lemon', 'fas fa-wine-glass'];
+var devices =  ['fas fa-desktop', 'fas fa-mobile-screen', 'fas fa-laptop', 'fas fa-microchip'];
+var danger =  ['fas fa-radiation', 'fas fa-biohazard', 'fas fa-hurricane', 'fas fa-person-walking-arrow-right'];
+var faces =  ['far fa-face-smile', 'far fa-face-laugh', 'far fa-face-grin', 'far fa-face-frown'];
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -17,13 +26,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-alert("OK");
+
 loadButtons();
 
 function loadButtons (){
+    faces.sort(() => Math.random() - 0.5) ;
     for (let btn = 0 ; btn < 4 ; btn ++ ) {
         currentId = "b-".concat(btn + 1);
         myIcon = document.getElementById(currentId);
-        myIcon.setAttribute('class', `${clouds[btn]}`);
+        myIcon.setAttribute('class', `${faces[btn]}`);
     }
 };
