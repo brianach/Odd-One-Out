50680b39cdfa09514082d1b6dd05a7a8e24f01bf
brianach <brianocualain@outlook.ie>
2023-02-09 16:19:50 +0000

    remove un-nessisary code from the event listener section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,7 +6,8 @@ var currPuzz = 0 ; //*counter to check which puzzle from the puzzles array is cu
 var scorePoint = 0 ;
 var puzzle = "" ;
 var currentId = "" ; //* id of the currently selected button 
-
+var gameLevel = 0 ; //* set initial value for level
+var nxtLvlFlag = 0 ; //* flag for level change
 
 //* array to hold arrays of icons for of all ten puzzles
 const puzzles = [alpha = ['fas fa-a', 'fas fa-i', 'fas fa-u', 'fas fa-z'],
@@ -27,20 +28,16 @@ document.addEventListener("DOMContentLoaded", function() {
             button.addEventListener("click", function(e) {
                 console.log(e.currentTarget.id); //* get the id of the object
                 currentId =  e.currentTarget.id ;
-                if (this.getAttribute("data-type") === "play-button") {
                     playLevel();
-                } else {
-                    alert(`Something went wrong!`);
-                }
             });
         }    
 });
 
-setPuzzOne(); //* load the first of the 10 puzzles
+setPuzzOne(); //* load the first puzzle array 
 
 function setPuzzOne(){
     //* intitialize the first puzzle
-    puzzles.sort(() => Math.random() - 0.5) ; //* randomize puzzle array so that the same ones dont load in the same order on a new gamne
+    puzzles.sort(() => Math.random() - 0.5) ; //* randomize puzzle array so that the same ones dont load in the same order every time
     puzzle = puzzles[currPuzz] ; //* set the first puzzle variable
     oddOne = puzzle[3] ; //* copy the last entry of the individual puzzle array which is the odd one out into a control variable
     puzzle.sort(() => Math.random() - 0.5) ; //* randomize the order of the buttons before rendering
@@ -49,52 +46,63 @@ function setPuzzOne(){
 
 
 function playLevel() {
-    playPuzz();
+
+    if (gameLevel === 0) {
+        playPuzz();
+    } else {
+        sayMessage = `Well Done. You've made it to round ${gameLevel} !`;
+        nextLevel(sayMessage);
+        currPuzz = scorePoint = 0;
+        setPuzzOne();
+    }
 }
 
 function playPuzz (){ //*play the currently presented puzzle
 
+    //*var buttonTimer = setTimeout("alert('Oh noes! you didnt click the button :(');", 5000);
     myIcon = document.getElementById(currentId); 
-    scoreFlag = parseInt(myIcon.getAttribute('odd-flag')); //* if the odd one out is selected increment the score
+    scoreFlag = parseInt(myIcon.getAttribute('odd-flag')); //* if the odd one out button is selected flag it 
     scoreCalc(scoreFlag);
-    currPuzz ++ ; //* increment the current puzzle flag so that the next puzzle loads afterwards
-    if (currPuzz >= 10) {
-        scoreCalc();
-    } else {   
-    puzzle = puzzles[currPuzz] ; //* set the first puzzle variable
-    oddOne = puzzle[3] ;  //* copy the last entry of the individual puzzle array which is the odd one out into a control variable
-    puzzle.sort(() => Math.random() - 0.5) ; //* randomize the order of the buttons 
-    newPuzz() ;
+    ++ currPuzz ; //* increment the puzzle count so that the next puzzle loads after score calculation
+    if ( currPuzz >= 9 && scorePoint >= 10 ) {
+        scoreCalc();     
+    } else { 
+    puzzle = puzzles[currPuzz] ; //* set the puzzle to the current puzzle array value
+    oddOne = puzzle[3] ;  //* copy the last entry of the current puzzle array which is the odd one out into a variable
+    puzzle.sort(() => Math.random() - 0.5) ; //* randomize the order of the buttons so that the odd one isn't always in the same location
+    newPuzz() ; //* call the function that renders the puzzle array buttons
     }
 }
 
 function newPuzz() {
-    for (let btn = 0 ; btn < 4 ; btn ++ ) { //* load an icon into each button from the puzzle array
+    for (let btn = 0 ; btn < 4 ; ++ btn ) { //* load an icon into each button from the puzzle array
         currentId = "b-".concat(btn + 1); //* create a variabble to check for the current button id
         myIcon = document.getElementById(currentId); //* get the button information of the current id
-        myIcon.setAttribute('class', `${puzzle[btn]} fa-5x`); //* assign the individual icon to the button
+        myIcon.setAttribute('class', `${puzzle[btn]} fa-5x`); //* assign the icon from the current puzzle array location to the button
         if ( oddOne === puzzle[btn] ) { //* check if the currently selected button matches the odd one out
             myIcon.setAttribute('odd-flag', 1) //* set the odd-flag attribute to 1 if the odd one out matches
         } else {
             myIcon.setAttribute('odd-flag', 0) //* if the wrong button is clicked then set the odd-flag to 0
         }
-        currentId = "" ; //* reinitialize current button id
+        currentId = "" ; //* reinitialize current button id for the next puzzle
     }    
 }
 
 function scoreCalc(){
+
+    if ( currPuzz >= 10 && scorePoint < 10 ) {
+        sayMessage = "Hard luck. Restart to try again !";
+        nextLevel(sayMessage);
+    } else if ( currPuzz >= 9 && scorePoint >= 10 ) {
+        gameLevel++ ;
+        playLevel();
+    }
+
     if ( scoreFlag === 1 ) {
         document.getElementById("scorebox").innerText = ++scorePoint ;
     } else { //* if the odd one out is not seletect decrement the score
         document.getElementById("scorebox").innerText = --scorePoint ;
-    }
-    if (scorePoint >= 10) {
-        sayMessage = "Well Done. You've made it to round 2 !";
-        nextLevel(sayMessage);
-    } else if (currPuzz >= 10) {
-        sayMessage = "Hard luck. Restart to try again !";
-        nextLevel(sayMessage);
-    }
+    } 
 }
 
 function nextLevel(sayMessage) {
