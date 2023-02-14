a4bb9dc177f9a591db5ceae390c27063c904b5c3
brianach <brianocualain@outlook.ie>
2023-02-14 19:00:26 +0000

    Fix: replaced conde containing fucntion declared within button listener loop  referencing an outer scoped variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,9 @@ document.querySelectorAll('button').forEach(occurence => {
   } );
 });
 
-/**document.addEventListener("DOMContentLoaded", function () {
+/** Code below replaced
+ * 
+ * document.addEventListener("DOMContentLoaded", function () {
   let buttons = document.getElementsByTagName("button");
   for (let button of buttons) {
     button.addEventListener("click", function (e) {
