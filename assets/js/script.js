0b4cdde2d14424f0df3cc9edaf29bf43e543fee5
brianach <brianocualain@outlook.ie>
2023-02-21 21:52:35 +0000

    Misc: tidy up code formatting and comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 //* initialize and set up game variables
-
 var oddOne = ""; //* holds the odd one out array entry
 var currPuzz = 0; //*counter to check which puzzle from the puzzles array is currently active
 var scorePoint = 0;
@@ -43,100 +42,105 @@ sec = 60;
 
 getUname();
 
-function getUname(){//* Get username
-document
-	.querySelector("#uname-in")
-	.addEventListener("keydown", function (event) {
-		if (event.key === "Enter") {
-			respondUser();
-		}
-	});
+function getUname() {
+  //* Get username
+  document
+    .querySelector("#uname-in")
+    .addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        respondUser();
+      }
+    });
 
-//* hide game play area section while username input is in progress
-let playArea = document.querySelector(".game-area");
-playArea.style.display = "none";
-let scoreArea = document.querySelector(".score-area");
-scoreArea.style.display = "none";
-document.getElementsByTagName("h3")[0].innerText =
-	"Type your username below then press enter.";
-
+  //* hide game play area section while username input is in progress
+  let playArea = document.querySelector(".game-area");
+  playArea.style.display = "none";
+  let scoreArea = document.querySelector(".score-area");
+  scoreArea.style.display = "none";
+  document.getElementsByTagName("h3")[0].innerText =
+    "Type your username below then press enter.";
 }
 
-function respondUser() { //* feedback for username input section
-	userName = document.getElementById("uname-in").value;
+function respondUser() {
+  //* feedback for username input section
+  userName = document.getElementById("uname-in").value;
 
-	if (userName.trim() == "") {
-		response.textContent = `You have to type in a username to play !`;
-		response.style.color = "#F6A38E";
-		setTimeout(clearName, 3000);
-	} else {
-		document.getElementsByTagName(
-			"h3"
-		)[0].innerText = `Hi ${userName}, click the lightbulb to play`;
-		quizPlay.addEventListener("click", clearUserInp);
-	}
-	function clearName() {
-		response.style.color = "white";
-	}
+  if (userName.trim() == "") {
+    response.textContent = `You have to type in a username to play !`;
+    response.style.color = "#F6A38E";
+    setTimeout(clearName, 3000);
+  } else {
+    document.getElementsByTagName(
+      "h3"
+    )[0].innerText = `Hi ${userName}, click the lightbulb to play`;
+    quizPlay.addEventListener("click", clearUserInp);
+  }
+  function clearName() {
+    response.style.color = "white";
+  }
 }
 
 //* Main game section
 
-function clearUserInp() { //* hide game play for user input section
-	let playArea = document.querySelector(".username-input");
-	playArea.style.display = "none";
-	revealGameArea();
+function clearUserInp() {
+  //* hide game play for user input section
+  let playArea = document.querySelector(".username-input");
+  playArea.style.display = "none";
+  revealGameArea();
 }
 
-function revealGameArea() { //* reveal the game area after user input section
-	let playArea = document.querySelector(".game-area");
-	playArea.style.display = "flex";
-	let scoreArea = document.querySelector(".score-area");
-	scoreArea.style.display = "flex";
-	startTimer(); //*set up the first instance of the timer
+function revealGameArea() {
+  //* reveal the game area after user input section
+  let playArea = document.querySelector(".game-area");
+  playArea.style.display = "flex";
+  let scoreArea = document.querySelector(".score-area");
+  scoreArea.style.display = "flex";
+  startTimer(); //*set up the first instance of the timer
 }
 
-gameButtons.forEach((button) => //* listen for mouse clicks in the main game area
-	button.addEventListener("click", buttonClickHandler)
-);
+gameButtons.forEach((
+  button //* listen for mouse clicks in the main game area
+) => button.addEventListener("click", buttonClickHandler));
 
-
-function buttonClickHandler(event) { //* button click logic decision code
-	if (timerIsLive) {
-		const clickedButtonText = event.currentTarget.id; //event.currentTarget.textContent;
-		refreshTimer(clickedButtonText); //* set the level timer
-	}
+function buttonClickHandler(event) {
+  //* button click logic decision code
+  if (timerIsLive) {
+    const clickedButtonText = event.currentTarget.id; //event.currentTarget.textContent;
+    refreshTimer(clickedButtonText); //* set the level timer
+  }
 }
 
 document.getElementById("roundbox").innerText = 0; //* set up the first quiz
 setPuzzOne();
 
-function setPuzzOne() { //* randomize the order of the puzzles and load the first puzzle array
-	altpuzzles.push(...puzzles); //* clone the puzzles array to preserve original
-	altpuzzles.sort(() => Math.random() - 0.5); //* randomize puzzles
-	puzzle = altpuzzles[currPuzz];
-	altpuzzle.push(...puzzle); //* set the first puzzle while preserving original
-	oddOne = altpuzzle[3]; //* copy the last entry of the puzzle array which is the odd one out
-	altpuzzle.sort(() => Math.random() - 0.5); //* randomize the order of the buttons before rendering
-	currPuzz++; //* increment in order to load new puzzle after intial load
-	newPuzz(); //* function to render puzzle icons
+function setPuzzOne() {
+  //* randomize the order of the puzzles and load the first puzzle array
+  altpuzzles.push(...puzzles); //* clone the puzzles array to preserve original
+  altpuzzles.sort(() => Math.random() - 0.5); //* randomize puzzles
+  puzzle = altpuzzles[currPuzz];
+  altpuzzle.push(...puzzle); //* set the first puzzle while preserving original
+  oddOne = altpuzzle[3]; //* copy the last entry of the puzzle array which is the odd one out
+  altpuzzle.sort(() => Math.random() - 0.5); //* randomize the order of the buttons before rendering
+  currPuzz++; //* increment in order to load new puzzle after intial load
+  newPuzz(); //* function to render puzzle icons
 }
 
-function newPuzz() { //* render the icons for the current puzzle
-	for (let btn = 0; btn < 4; ++btn) {
-		//* load an icon into each button from the puzzle array
-		currentId = "b-".concat(btn + 1); //* load variable with the current button id
-		myIcon = document.getElementById(currentId); //* get the button information of the current id
-		myIcon.setAttribute("class", `${altpuzzle[btn]} fa-5x`); //* assign the icon from the current puzzle array location to the button
-		if (oddOne === altpuzzle[btn]) {
-			//* check if the currently selected button matches the odd one out
-			myIcon.setAttribute("odd-flag", 1); //* set the odd-flag attribute to 1 if the odd one out matches
-		} else {
-			myIcon.setAttribute("odd-flag", 0); //* if the wrong button is clicked then set the odd-flag to 0
-		}
-	}
-	currentId = ""; //* reinitialize current button id for the next puzzle
-	altpuzzle.length = 0; //* reinitialize current array for the next puzzle
+function newPuzz() {
+  //* render the icons for the current puzzle
+  for (let btn = 0; btn < 4; ++btn) {
+    //* load an icon into each button from the puzzle array
+    currentId = "b-".concat(btn + 1); //* load variable with the current button id
+    myIcon = document.getElementById(currentId); //* get the button information of the current id
+    myIcon.setAttribute("class", `${altpuzzle[btn]} fa-5x`); //* assign the icon from the current puzzle array location to the button
+    if (oddOne === altpuzzle[btn]) {
+      //* check if the currently selected button matches the odd one out
+      myIcon.setAttribute("odd-flag", 1); //* set the odd-flag attribute to 1 if the odd one out matches
+    } else {
+      myIcon.setAttribute("odd-flag", 0); //* if the wrong button is clicked then set the odd-flag to 0
+    }
+  }
+  currentId = ""; //* reinitialize current button id for the next puzzle
+  altpuzzle.length = 0; //* reinitialize current array for the next puzzle
 }
 
 /**function playRound(currentId) {
@@ -145,116 +149,121 @@ function newPuzz() { //* render the icons for the current puzzle
 }*/
 
 function playPuzz(currentId) {
-	//*play the currently presented puzzle
+  //*play the currently presented puzzle
 
-	scoreFlag = parseInt(
-		document.getElementById(currentId).getAttribute("odd-flag")
-	); //* get the flag value of the clicked button (0 or 1)
-	scoreCalc(scoreFlag); //* send the result to the score calculation function
+  scoreFlag = parseInt(
+    document.getElementById(currentId).getAttribute("odd-flag")
+  ); //* get the flag value of the clicked button (0 or 1)
+  scoreCalc(scoreFlag); //* send the result to the score calculation function
 
-	if (currPuzz === 10) {
-		return false;
-	}
-	puzzle = altpuzzles[currPuzz]; //* set the puzzle to the current puzzle array value while preserving original
-	altpuzzle.push(...puzzle);
-	oddOne = altpuzzle[3]; //* copy the last entry of the current puzzle array which is the odd one out into a variable
-	altpuzzle.sort(() => Math.random() - 0.5); //* randomize the order of the buttons so that the odd one isn't always in the same location
-	newPuzz(); //* call the function that renders the puzzle array buttons
+  if (currPuzz === 10) {
+    return false;
+  }
+  puzzle = altpuzzles[currPuzz]; //* set the puzzle to the current puzzle array value while preserving original
+  altpuzzle.push(...puzzle);
+  oddOne = altpuzzle[3]; //* copy the last entry of the current puzzle array which is the odd one out into a variable
+  altpuzzle.sort(() => Math.random() - 0.5); //* randomize the order of the buttons so that the odd one isn't always in the same location
+  newPuzz(); //* call the function that renders the puzzle array buttons
 
-	++currPuzz; //* increment the puzzle count so that the next puzzle loads after score calculation
+  ++currPuzz; //* increment the puzzle count so that the next puzzle loads after score calculation
 }
 
 function scoreCalc() {
-	if (scoreFlag === 1) {
-		document.getElementById("scorebox").innerText = ++scorePoint;
-	} else {
-		//* if the odd one out is not seletect decrement the score
-		document.getElementById("scorebox").innerText = --scorePoint;
-	}
+  if (scoreFlag === 1) {
+    document.getElementById("scorebox").innerText = ++scorePoint;
+  } else {
+    //* if the odd one out is not seletect decrement the score
+    document.getElementById("scorebox").innerText = --scorePoint;
+  }
 
-	if (currPuzz >= 10 && scorePoint < 10) {
-		resultContainer.innerText = "Hard luck. Try again !";
+  if (currPuzz >= 10 && scorePoint < 10) {
+    resultContainer.innerText = "Hard luck. Try again !";
 
-		winSound = new playSound("assets/snd/foghorn.mp3"); //* party trumpet sound
-		winSound.play();
+    winSound = new playSound("assets/snd/foghorn.mp3"); //* party trumpet sound
+    winSound.play();
 
-		setTimeout(reStart, 2000);
-	} else if (currPuzz >= 10 && scorePoint >= 10) {
-		gameRound++;
+    setTimeout(reStart, 2000);
+  } else if (currPuzz >= 10 && scorePoint >= 10) {
+    gameRound++;
 
-	  winSound = new playSound("assets/snd/partypop.mp3"); //* party trumpet sound
-		winSound.play();
+    winSound = new playSound("assets/snd/partypop.mp3"); //* party trumpet sound
+    winSound.play();
 
-		setTimeout(nextRound, 2000); //* wait 5 seconds to enjoy win before next round
-	}
+    setTimeout(nextRound, 2000); //* wait 5 seconds to enjoy win before next round
+  }
 }
 
 function nextRound() {
-	currPuzz = scorePoint = 0; //* reset the puzzle count to 0
-	document.getElementById("roundbox").innerText = gameRound; //* increment the round
-	document.getElementById("scorebox").innerText = scorePoint; //* reset score to 0 for next round
-	altpuzzles.length = 0; //* reset nested array for new round
-	setPuzzOne(); //* call the first puzzle function
+  currPuzz = scorePoint = 0; //* reset the puzzle count to 0
+  document.getElementById("roundbox").innerText = gameRound; //* increment the round
+  document.getElementById("scorebox").innerText = scorePoint; //* reset score to 0 for next round
+  altpuzzles.length = 0; //* reset nested array for new round
+  setPuzzOne(); //* call the first puzzle function
 }
 
 function playSound(src) {
-	this.sound = document.createElement("audio");
-	this.sound.src = src;
-	this.sound.setAttribute("preload", "auto");
-	this.sound.setAttribute("controls", "none");
-	this.sound.style.display = "none";
-	document.body.appendChild(this.sound);
-	this.play = function () {
-		this.sound.play();
-	};
-	this.stop = function () {
-		this.sound.pause();
-	};
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function () {
+    this.sound.play();
+  };
+  this.stop = function () {
+    this.sound.pause();
+  };
 }
 
-function refreshTimer(buttonText) { //* start the countdown progress bar
-		switch(toggle){
-			case true: //* will run if the toggle value is true after the timer was reset or at the start
-				startTimer();
-				break;
-			case false:
-				resetTimer(); //* will run if the tiggle value is fase ie after resert button is clicked
-				break;
-		}
-	playPuzz(buttonText);	
+function refreshTimer(buttonText) {
+  //* start the countdown progress bar
+  switch (toggle) {
+    case true: //* will run if the toggle value is true after the timer was reset or at the start
+      startTimer();
+      break;
+    case false:
+      resetTimer(); //* will run if the tiggle value is fase ie after resert button is clicked
+      break;
+  }
+  playPuzz(buttonText);
 }
 
-function stopGame() { //* game over alert
+function stopGame() {
+  //* game over alert
 
-	resultContainer.innerText = "Hard Luck. You're out of time. Try again !";
-	timerIsLive = false;
-	clearTimeout(gameTimeout);
+  resultContainer.innerText = "Hard Luck. You're out of time. Try again !";
+  timerIsLive = false;
+  clearTimeout(gameTimeout);
 }
 
 //* progress timer code functions
 
 function startTimer() {
-	sec = 59;
-	countdown = setInterval(currentTime, 100);
-	toggle = false; //* set to false once timer starts so will trigger reset if button is clicked
+  sec = 59;
+  countdown = setInterval(currentTime, 100);
+  toggle = false; //* set to false once timer starts so will trigger reset if button is clicked
 }
 
-function resetTimer(){
-	clearInterval(countdown);
-	toggle = true; //* reset the toggle after resetting the countdown
+function resetTimer() {
+  clearInterval(countdown);
+  toggle = true; //* reset the toggle after resetting the countdown
   width = 1;
-	elem.style.width = width;
+  elem.style.width = width;
   startTimer();
 }
 
 function currentTime() {
-	if (sec === 0){alert(`${sec}'s left`);}
-	sec--;  //* the sec variable holds the quiz timer value and alerts if less than 0
-	  
-	width++;
-	elem.style.width = width + "%"; //* draws the progress bar on the page   
+  if (sec === 0) {
+    alert(`${sec}'s left`);
+  }
+  sec--; //* the sec variable holds the quiz timer value and alerts if less than 0
+
+  width++;
+  elem.style.width = width + "%"; //* draws the progress bar on the page
 }
 
-function reStart() { //* this loads the entire game from scratch
-	location.reload();
+function reStart() {
+  //* this loads the entire game from scratch
+  location.reload();
 }
